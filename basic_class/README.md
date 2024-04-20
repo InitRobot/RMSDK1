74bf9720dbb0708d71e9927cb2c4ad8a499815ce
yuanzl <vict2008or0411@163.com>
2024-04-20 13:13:51 +0800

    完成 - class UDP_connect - def solve_game - def solve_key - solve_key_name - def solve_gimbal - def solve_chassis_position

--- basic_class/README.md
+++ basic_class/README.md
@@ -1,6 +1,6 @@
 # README
 
-### class：TCP_connect
+### class TCP_connect
 
 用于TCP相关连接，断开，消息接收等功能
 
@@ -34,9 +34,51 @@
 
 
 
+### class UDP_connect
+
+用于UCP相关连接，断开，消息接收等功能
+
+##### def connect
+
+与机器人控制命令端口建立 UDP 连接
+
+##### def disconnect
+
+与机器人控制命令端口断开UDP 连接
+
+##### def try_get
+
+这个函数默认等待5秒钟，如果在这个时间内没有收到机器人的返回结果，就会立即返回'no_OUT'。如果收到了机器人的返回结果，就会解码并返回结果字符串。
+
+
+
+### def solve_game
+
+用于解析赛事数据推送
+
+### def solve_key
+
+用于从赛事数据推送中获得键位
+
+### solve_key_name
+
+将获得键位转换为真实名称
+
+### def solve_gimbal
+
+### def solve_chassis_position
+
 
 
 # 日志
 
 - 20240420
-  - 12:44 完成class TCP_connection
+  - 12:44 完成
+    - class TCP_connection
+  - 13:12 完成
+    - class UDP_connect
+    - def solve_game
+    - def solve_key
+    - solve_key_name
+    - def solve_gimbal
+    - def solve_chassis_position
--- basic_class/connection.py
+++ basic_class/connection.py
@@ -99,3 +99,160 @@ class TCP_connection:
         self.IN_OUT("command;", timeout = timeout,printing = self.printing)
         self.IN_OUT("quit;", timeout = timeout,printing = self.printing)#以免图传卡住
         self.IN_OUT("command;", timeout = timeout,printing = self.printing)
+
+
+class UDP_connection:
+    # USB 模式下，机器人默认 IP 地址为 192.168.42.2, 消息推送端口号为 40924
+    host = "0.0.0.0"
+    port = 40924
+    printing = True
+    connection = False
+
+    def __init__(self, printing_ = True):
+        self.printing = printing_
+
+    def connect(self):# 与机器人控制命令端口建立 UDP 连接
+        self.address = (self.host, int(self.port))
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        if self.printing:
+            print("Connecting_UDP...")
+
+        self.udp_socket.bind(self.address)
+
+        if self.printing:
+            print("UDP_Connected!")
+
+    def disconnect(self):
+        if self.printing:
+            print("UDP disconnecting...")
+        self.udp_socket.close()
+        if self.printing:
+            print("UDP disconnected!")
+
+    def try_get(self,timeout=5):#这个函数默认等待5秒钟，如果在这个时间内没有收到机器人的返回结果，就会立即返回'no_OUT'。如果收到了机器人的返回结果，就会解码并返回结果字符串。
+        result = ''
+        try:
+            # 设置超时时间
+            ready = select.select([self.udp_socket], [], [], timeout)
+            #print(ready)
+            if ready[0]:
+                # 如果有可读数据，接收并解码
+               #print("hearing...")
+                buf = self.udp_socket.recv(1024)
+                result = buf.decode('utf-8')
+            else:
+                result = 'no_OUT'
+
+        except socket.error as e:
+            if self.printing:
+                print("Error receiving :", e)
+
+            sys.exit(1)
+
+        if self.printing:
+            print(result)
+
+        return result
+
+def solve_game(msg, printing = True):#用于解析赛事数据推送
+    result = ''
+    if msg[0:14] == 'game msg push ' and msg[-1] == ';':
+        #print('right_start')
+        info = msg[15:-2]
+        #print(info)
+        info_list = info.split(', ')
+        #print(info_list)
+        info_list_int = [ int(i) for i in info_list ]
+        #print(info_list_int)
+        result = info_list_int
+    else:
+        if printing:
+            print('please give a game msg push')
+    return result
+
+def solve_key(msg, printing = True):#用于从赛事数据推送中获得键位
+    result = []
+    key_n = msg[6]
+    if printing:
+        print(key_n)
+    if key_n != 0:
+        keys = msg[0 - key_n: ]
+        if printing:
+            print(keys)
+            print(type(keys))
+        result = keys
+    return result
+
+def solve_key_name(keys):#将获得键位转换为真实名称
+    result = []
+    key_name_list = []
+    for key in keys:
+        if key >= 48 and key <= 90:
+            key_name_list.append(chr(key))
+        elif key == 8:
+            key_name_list.append("Space")
+        elif key == 9:
+            key_name_list.append("Tab")
+        elif key == 16:
+            key_name_list.append("Shift")
+        elif key == 17:
+            key_name_list.append("Ctrl")
+        elif key == 18:
+            key_name_list.append("Alt")
+        elif key == 20:
+            key_name_list.append("Caps")
+    result = key_name_list
+    return result
+
+def solve_gimbal(msg, printing = True):
+    result = ''
+    if msg[-1] == ';':
+        if printing:
+            print('right_start')
+        info = msg[:-2]
+        if printing:
+            print(info)
+        info_list = info.split(' ')
+        if printing:
+            print(info_list)
+        info_list_int = [ float(i) for i in info_list ]
+        if printing:
+            print(info_list_int)
+        result = info_list_int
+    else:
+        if printing:
+            print('please give a gimbal msg push')
+    return result
+
+def solve_chassis_position(msg, printing = True):
+    result = ''
+    if msg[0:22] == 'chassis push position ' and msg[-1] == ';':
+        #print('right_start')
+        info = msg[22:-3]
+        if printing:
+            print(info)
+        info_list = info.split(' ')
+        if printing:
+            print(info_list)
+        info_list_float = []
+        for i in info_list:
+            if printing:
+                print(i)
+            if i != "-0.000" and i != "0.000":
+                if printing:
+                    print("float")
+                if i[0] == '-':
+                    info_list_float.append(-float(i[1:]))
+                else:
+                    info_list_float.append(float(i))
+            else:
+                info_list_float.append(0)
+        if printing:
+            print(info_list_float)
+        result = info_list_float
+    else:
+        if printing:
+            print('please give a chassis push position push')
+    return result
+
