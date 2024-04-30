28923f6c6292d8101d1fcfb560befe7489adb28c
yuanzl <vict2008or0411@163.com>
2024-04-30 19:16:05 +0800

    test

--- basic_class/Chassis_Solve.py
+++ basic_class/Chassis_Solve.py
@@ -13,13 +13,16 @@ def Stright_Solve(TCP, degree, keys, printing=True):
 	# 对应    左右
 	wheel = [0, 0,  # 前(head)
 	         0, 0]  # 后(tail)
+	# 对应    左右
+	wheel2 = [0, 0,  # 前(head)
+	         0, 0]  # 后(tail)
 	#print(degree)
 	if abs(degree) >= 25:
-		kp = 0.00001
-		wheel[0] = degree * kp
-		wheel[1] = 0 - degree * kp
-		wheel[2] = degree * kp
-		wheel[3] = 0 - degree * kp
+		kp = 0.01
+		wheel2[0] = degree * kp
+		wheel2[1] = 0 - degree * kp
+		wheel2[2] = degree * kp
+		wheel2[3] = 0 - degree * kp
 	if 'W' in keys:
 		wheel = [(i + 1) for i in wheel]
 	if 'A' in keys:
@@ -36,9 +39,9 @@ def Stright_Solve(TCP, degree, keys, printing=True):
 		wheel[3] += 1
 	for i in range(len(wheel)):
 		if wheel[i] > 0:
-			wheel[i] = 1
+			wheel[i] = 1 + wheel2[i]
 		elif wheel[i] < 0:
-			wheel[i] = -1
+			wheel[i] = -1 + wheel2[i]
 	if printing:
 		print(wheel)
 	result = wheel
