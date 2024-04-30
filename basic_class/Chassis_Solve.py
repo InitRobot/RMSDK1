71d4111e75ed929134b6b3b4758a4d01708d0cb2
yuanzl <vict2008or0411@163.com>
2024-04-30 19:29:55 +0800

    test

--- basic_class/Chassis_Solve.py
+++ basic_class/Chassis_Solve.py
@@ -110,7 +110,7 @@ def Disk_solve(TCP, keys, degree, spin=1, printing=True):
 	
 	return wheel_spin
 
-speed = 10
+speed = 30
 
 def move(TCP, wheel, printing=True):
 	wheel = [i * 1000 / 100 * speed for i in wheel]
