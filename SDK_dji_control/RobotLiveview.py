8f3811926dcec38740a5bae85542fc323a77516d
victorchenjiantian <vict2008or0411@163.com>
2024-05-05 16:34:01 +0800

    print("auto_aim")

--- SDK_dji_control/RobotLiveview.py
+++ SDK_dji_control/RobotLiveview.py
@@ -152,6 +152,7 @@ class RobotLiveview(object):
             image = PImage.fromarray(frame)
             img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
             #auto_aim()?
+            print("auto_aim")
             auto_aim.auto_aim(img)
             #cv2.imshow("Liveview", img)
             #cv2.waitKey(1)
