72003484c2dd4bcd5e7751e12fc6e78b48f99220
victorchenjiantian <vict2008or0411@163.com>
2024-05-05 17:35:51 +0800

    tab

--- SDK_dji_control/main.py
+++ SDK_dji_control/main.py
@@ -102,10 +102,9 @@ def video_test():
 		print(robot_liveview.get_target())
 		robot.send_data('game_msg on;')
 		if float(pos_arr[0]) != 0:
-        	yaw = int((posx/360-1)*55)
-       		pitch = -int((posy/180-1)*55)
-        	print(yaw,pitch)
-        	#ep_gimbal.move(yaw=yaw, pitch=pitch, pitch_speed=20, yaw_speed=20)
+			yaw = int((posx/360-1)*55)
+			pitch = -int((posy/180-1)*55)
+			print(yaw,pitch)
 			robot.send_data('gimbal move p '+ pitch +' y '+ yaw +';')
 		#print(msg)
 		#if msg:
