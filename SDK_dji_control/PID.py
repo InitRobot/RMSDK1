8ea4ff2ecdebed625466eb3efe2f235ea091abab
victorchenjiantian <vict2008or0411@163.com>
2024-05-08 06:30:29 +0800

    PID.py

---
+++ SDK_dji_control/PID.py
@@ -0,0 +1,21 @@
+
+Kp = [0,0]
+Ki = 
+Kd = 
+
+def PID(type_choice,error):
+
+class PID:
+    sum_error = 0
+    self.last_error = 0
+    def __init__(self,Kp,Ki,Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+    
+    def control(self,error):
+        d_error = 2 * error - self.last_error
+        self.sum_error += error
+        result = self.Kp * error + self.Ki * self.sum_error + self.Kd * d_error 
+        self.last_error = error
+        return result
