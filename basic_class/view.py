e321be55da5984fc57450c150ccd1878b65fd942
victorchenjiantian <vict2008or0411@163.com>
2024-05-13 06:50:21 +0800

    test

--- basic_class/view.py
+++ basic_class/view.py
@@ -3,8 +3,8 @@ import socket
 import struct
 import time
 
-HOST = '10.42.0.1'  # 电脑端地址
-PORT = 9999
+# HOST = '10.42.0.1'  # 电脑端地址
+# PORT = 9999
 # server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # server.connect((HOST, PORT))
 capture = cv2.VideoCapture('/dev/video11')
@@ -19,11 +19,12 @@ capture.set(h, 768)
 try:
     while True:
         a = time.time()
+        print(a)
         success, frame = capture.read()
         result, imgencode = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])  # 编码
         # server.sendall(imgencode)  # 发送视频帧数据
         imgencode = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        cv2.imshow("Liveview", img)
+        cv2.imshow("Live view", img)
         cv2.waitKey(1)
         b = time.time()
         print(b - a)
@@ -31,4 +32,4 @@ try:
 except Exception as e:
     print(e)
     capture.release()
-    #server.close()
+    # server.close()
