aa52e68064731cb8ec0ca66b663cddd99f35ecb4
victorchenjiantian <vict2008or0411@163.com>
2024-05-13 18:12:05 +0800

    test0

--- basic_class/view.py
+++ basic_class/view.py
@@ -7,7 +7,7 @@ import time
 # PORT = 9999
 # server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # server.connect((HOST, PORT))
-capture = cv2.VideoCapture('3')
+capture = cv2.VideoCapture(0)
 
 w = 3
 h = 4
