6804c850b523d5e3e1e13f77ad4517eaa11f53bd
victorchenjiantian <vict2008or0411@163.com>
2024-05-14 17:11:26 +0800

    test

--- basic_class/get_view.py
+++ basic_class/get_view.py
@@ -4,19 +4,19 @@ import cv2
 class vidio:
     def __init__(self):
         self.cap = cv2.VideoCapture(1)
-        #fps = cap.get(cv2.CAP_PROP_FPS)
+        # fps = cap.get(cv2.CAP_PROP_FPS)
 
-    def get_vidio(self, printing = False):
-        success, frame = self.cap.read_latest_frame()
+    def get_vidio(self, printing=False):
+        success, frame_ = self.cap.read_latest_frame()
         if not success:
             if printing:
                 print("fail")
             return False
-        return frame
+        return frame_
 
 
 if __name__ == '__main__':
-    my_vidio = vidio
+    my_vidio = vidio()
     while True:
-        frame = my_vidio.get_vidio(printing = True)
+        frame = my_vidio.get_vidio(printing=True)
         print(frame)
