0fcc6d9cb47ca29231f1600f3b140343ad6bf253
victorchenjiantian <vict2008or0411@163.com>
2024-05-14 17:31:36 +0800

    pi_auto_aim

---
+++ basic_class/pi_auto_aim.py
@@ -0,0 +1,26 @@
+import get_view
+import re
+from ultralytics import YOLO
+
+class aim:
+    model = YOLO('../../RM-yolo/runs/best.pt')
+    def __init__(self):
+        self.vidio = get_view.vidio()
+
+    def target(self,blue):
+        if blue:
+            pass
+        else:
+            img = self.vidio.get_vidio(printing=True)
+            results = model(img)
+            for r in results:
+                boxes = r.boxes
+            pos = boxes.xywh
+            pos = str(pos)
+            pos_arr = re.findall("\d+\.?\d*", pos)
+            print(pos_arr)
+
+if __name__ == '__main__':
+    a = aim()
+    while True:
+        a.target(False)
