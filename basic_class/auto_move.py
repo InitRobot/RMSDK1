092f11f2030f1e5441afa103da2ab086fe8c6b8c
victorchenjiantian <vict2008or0411@163.com>
2024-05-15 18:54:00 +0800

    i'm dead

--- basic_class/auto_move.py
+++ basic_class/auto_move.py
@@ -1,5 +1,6 @@
 import time
 import math
+import PID
 
 
 class Root:
@@ -7,21 +8,56 @@ class Root:
     p_parameter_list = []  # 各阶段的移动种类的参数；0:0,1:down,2:left,3:R,4:R,5:R,6:R,7:up,8:right
     p_dis_list = []  # 各阶段的移动距离
     time_cnt_list = [0]  # 各阶段累计时间
+    x_list = [0]  # 各阶段x
+    y_list = [0]  # 各阶段y
 
     def __init__(self, p_type, p_parameter, speed):
         self.p_type_list = p_type
         self.p_parameter_list = p_parameter
         self.speed = speed
+
         i = 0
         for type_ in self.p_type_list:  # 更新距离与时间
             parameter_ = self.p_parameter_list[i]
             if 2 < type_ < 7 or 9 < type_ < 13:
                 dis = math.pi * parameter_ / 2
                 self.p_dis_list.append(dis)
+                if type_ == 3:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] + parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] - parameter_)
+                elif type_ == 4:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] - parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] - parameter_)
+                elif type_ == 5:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] + parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] - parameter_)
+                elif type_ == 6:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] - parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] - parameter_)
+                elif type_ == 9:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] - parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] + parameter_)
+                elif type_ == 10:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] + parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] + parameter_)
+                elif type_ == 11:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] + parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] + parameter_)
+                elif type_ == 12:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] - parameter_)
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] + parameter_)
                 self.time_cnt_list.append(self.time_cnt_list[len(self.time_cnt_list) - 1] + dis / self.speed)
             else:
                 dis = self.p_parameter_list[i]
                 self.p_dis_list.append(dis)
+                if type_ == 1:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] - parameter_)
+                elif type_ == 2:
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] - parameter_)
+                elif type_ == 7:
+                    self.x_list.append(self.x_list[len(self.x_list) - 1] + parameter_)
+                elif type_ == 8:
+                    self.y_list.append(self.y_list[len(self.y_list) - 1] + parameter_)
                 self.time_cnt_list.append(self.time_cnt_list[len(self.time_cnt_list) - 1] + dis / self.speed)
             i += 1
 
@@ -33,49 +69,46 @@ class Root:
                 break
             i += 1
         # print(self.time_cnt_list)
+        x = self.x_list[i]
+        y = self.y_list[i]
+
         try:
             degree = math.pi / 2 * (self.time_cnt_list[i] - t) / (self.time_cnt_list[i] - self.time_cnt_list[i - 1])
+            # x = math.pi / 2 * (self.time_cnt_list[i] - t) / (self.time_cnt_list[i] - self.time_cnt_list[i - 1])
         except IndexError:
             print("IndexError")
             return False
         print("degree", degree)
+        deg = 0
         if self.p_type_list[i - 1] == 1:  # down ok
-            return math.pi * 1
+            deg = math.pi * 1
         elif self.p_type_list[i - 1] == 2:  # left ok
-            return math.pi * 0.5
+            deg = math.pi * 0.5
         elif self.p_type_list[i - 1] == 3:  # down 7 ok
-            return math.pi - degree
+            deg = math.pi - degree
         elif self.p_type_list[i - 1] == 4:  # down r ok
-            return math.pi + degree
+            deg = math.pi + degree
         elif self.p_type_list[i - 1] == 5:  # down l ok
-            return 0.5 * math.pi + degree
+            deg = 0.5 * math.pi + degree
         elif self.p_type_list[i - 1] == 6:  # down j ok
-            return 1.5 * math.pi - degree
+            deg = 1.5 * math.pi - degree
         elif self.p_type_list[i - 1] == 7:  # up ok
-            return 2 * math.pi
+            deg = 2 * math.pi
         elif self.p_type_list[i - 1] == 8:  # right ok
-            return math.pi * 1.5
+            deg = math.pi * 1.5
         elif self.p_type_list[i - 1] == 9:  # up 7 ok
-            return 1.5 * math.pi + degree
+            deg = 1.5 * math.pi + degree
         elif self.p_type_list[i - 1] == 10:  # up r ok
-            return 0.5 * math.pi - degree
+            deg = 0.5 * math.pi - degree
         elif self.p_type_list[i - 1] == 11:  # up l
-            return 0 - degree
+            deg = 0 - degree
         elif self.p_type_list[i - 1] == 12:  # up j
-            return degree
+            deg = degree
         print("no_move")
-        return False
+        return x, y, deg
 
 
 class Auto:
-    kp_x = 4
-    ki_x = 0.01
-    kd_x = 2
-
-    kp_y = 6
-    ki_y = 0.01
-    kd_y = 2
-
     # type_list = [1, 6, 4, 1, 6, 5]
     type_list = [1]
     parameter_list = [1]
@@ -83,6 +116,8 @@ class Auto:
     speed = 1
 
     def __init__(self, tcp, udp, printing=True):  #
+        self.x_pid = PID.PID(4, 0, 0)
+        self.y_pid = PID.PID(4, 0, 0)
         self.msg = None
         self.tcp = tcp
         self.udp = udp
@@ -129,13 +164,16 @@ class Auto:
             # print(self.msg)
             x_y = self.solve_chassis_position(printing=False)
             print("xy:", x_y)
+
             now_time = time.perf_counter() - start_time
-            #bprint("time:-------", now_time)
-            dir_ = self.root.get_stage(now_time)
+            # print("time:-------", now_time)
+            target_x, target_y, dir_ = self.root.get_stage(now_time)
+            x_out = self.x_pid.pid_control(target_x - x_y[0])
+            y_out = self.y_pid.pid_control(target_y - x_y[1])
             print(round(dir_ / math.pi * 180, 2))
             x = self.speed * math.sin(dir_)
             y = self.speed * math.cos(dir_)
-            self.tcp.IN_OUT("chassis speed x " + str(y) + " y " + str(x) + ";", printing=printing)
+            self.tcp.IN_OUT("chassis speed x " + str(y+x_out) + " y " + str(x+y_out) + ";", printing=printing)
             # print(result)
             # print("dir", dir_)
             if not dir_:
